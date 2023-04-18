37fc41335b6cc23244713fe89c72b9eccc80f642
tataputri <105484949+tataputri@users.noreply.github.com>
2023-04-19 06:16:33 +0700

    Create custom.js

---
+++ static/js/custom.js
@@ -0,0 +1,16 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+atOptions = {
+		'key' : '90b57d8bd827902ed9b631b8db146bfd',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/90b57d8bd827902ed9b631b8db146bfd/invoke.js"></scr' + 'ipt>');
+	// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
